6b51c810f619c21677edb14dfc6767992e983b48
VincentBernet <vincent.bernet@efrei.net>
2022-11-24 20:42:23 +0100

    feat: initialize front-end project

---
+++ front-end/src/commons/components/MusicTable.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const MusicTable = (): React.ReactElement => {
+	return (
+		<table>
+			<tr>
+				<th>Titre</th>
+				<th>Artiste</th>
+				<th>Tag 1</th>
+				<th>Tag 2</th>
+				<th>Tag 3</th>
+				<th>Tag 5</th>
+				<th>Tag 6</th>
+				<th>Tag 7</th>
+				<th>Tag 8</th>
+				<th>Tag 9</th>
+				<th>Tag 10</th>
+			</tr>
+			<tr>
+				<td>As It Was</td>
+				<td>10.01.2014</td>
+			</tr>
+			<tr>
+				<td>Février</td>
+				<td>10.01.2014</td>
+			</tr>
+		</table>
+	);
+};
+
+export default MusicTable;
---
+++ front-end/src/features/home/pages/homePage.tsx
@@ -0,0 +1,12 @@
+import MusicTable from '../../../commons/components/MusicTable';
+import React from 'react';
+
+const HomePage = (): React.ReactElement => {
+    return (
+        <>
+            <MusicTable />
+        </>
+    );
+};
+
+export default HomePage;
