336ac9c28d47f6a864aa86a66fbb19b64b207756
VincentBernet <vincent.bernet@efrei.net>
2022-12-26 22:24:42 +0100

    feat: begining of connecting backend spotify auth with the front

--- front-end/src/features/login/commons/callApi.ts
+++ front-end/src/features/login/commons/callApi.ts
@@ -1,5 +1,26 @@
 import { playlistListType } from './playlistDto';
 
+export const getAuthentification = async (): Promise<any> => {
+	let json: any = 'empty';
+	console.log('Appel du back via la fonctione getAllPlaylist');
+	try {
+		const endpointUrl = 'https://own-proxy-cors.herokuapp.com/https://track-it-spotify.herokuapp.com/auth/login';
+		await fetch(endpointUrl).then((response) => response
+			.json()
+			.then((data) => ({
+				data,
+				status: response.status,
+			}))
+			.then((res) => {
+				json = res.data;
+			}));
+	} catch (error) {
+		console.log(`We got some error with the api call chef: ${error}`);
+		json = `We got some error with the api call chef: ${error}`;
+	}
+	return json;
+};
+
 export const getAllPlaylist = async (): Promise<playlistListType> => {
 	let json: playlistListType = [{name: 'Initialisation'}];
 	console.log('Appel du back via la fonctione getAllPlaylist');
--- front-end/src/features/login/pages/LoginPage.tsx
+++ front-end/src/features/login/pages/LoginPage.tsx
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
-import { getAllPlaylist } from '../commons/callApi';
+import { getAllPlaylist, getAuthentification } from '../commons/callApi';
 
 const LoginPage = (): React.ReactElement => {
 	const [playlistList, setPlaylistList] = useState([{name: 'test'},]);
 	const [showPlaylistList, setShowPlaylistList] = useState(false);
 
 	async function onClickLogin() {
-		const data = await getAllPlaylist();
+		// const data = await getAllPlaylist();
+		const data = await getAuthentification();
 		setPlaylistList(data);
 		setShowPlaylistList(true);
 	}
   
 	return (
 		<>
-			<p>Track It</p>
-			<p>Your interface to organize your music, currently supporting only Spotify</p>
-			<p>A project by Romain Jouffreau and Vincent Bernet</p>
-			<button onClick={onClickLogin}>LOG IN WITH SPOTIFY</button>
-			{showPlaylistList ? (
-				<div>{playlistList[0].name}</div>
-			) : (
-				<div>Do not showPlaylistList</div>
-			)}
+			<div>
+				<h1>Track It</h1>
+				<p>
+					<span>Your interface to organize your music, currently supporting only Spotify</span> <br/>
+					<span>A project by Romain Jouffreau and Vincent Bernet</span>
+				</p>
+				<button onClick={onClickLogin}>LOG IN WITH SPOTIFY</button>
+			</div>
+
+			{/*showPlaylistList && <div>{playlistList.map(playlist => playlist.name + ' | ')}</div>*/}
 		</>
 	);
 };
