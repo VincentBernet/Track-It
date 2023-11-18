9780b0f2e1719a71081dbbf57aa13825b123e925
unknown <vincent.bernet@efrei.net>
2023-11-18 23:35:01 +0100

    fix: fix typing issue in mock that stopped build

--- frontend/src/commons/spotify/responsesMock.ts
+++ frontend/src/commons/spotify/responsesMock.ts
@@ -1,4 +1,4 @@
-import { playlist } from "./responsesTypes";
+import { playlist, track } from "./responsesTypes";
 
 const PlaylistsMock: playlist[] = [
     {
@@ -95,116 +95,150 @@ const PlaylistsMock: playlist[] = [
 
 const TracksMock: track[] = [
     {
-        id: "1",
         album: {
+            album_type: "album",
+            total_tracks: 12,
+            available_markets: ["US", "CA", "UK"],
+            external_urls: {
+                spotify: "https://example.com/album1",
+            },
+            href: "https://api.spotify.com/v1/albums/album1",
+            id: "album1",
             images: [],
             name: "Album 1",
+            release_date: "2023-01-01",
+            release_date_precision: "day",
+            restrictions: {
+                reason: "Some reason",
+            },
+            type: "album",
+            uri: "spotify:album:album1",
+            artists: [
+                {
+                    external_urls: {
+                        spotify: "https://example.com/artist1",
+                    },
+                    href: "https://api.spotify.com/v1/artists/artist1",
+                    id: "artist1",
+                    name: "Artist 1",
+                    type: "artist",
+                    uri: "spotify:artist:artist1",
+                },
+            ],
+        },
+        artists: [],
+        disc_number: 1,
+        duration_ms: 240000,
+        explicit: false,
+        external_urls: {
+            spotify: "https://example.com/track1",
+        },
+        href: "https://api.spotify.com/v1/tracks/track1",
+        id: "track1",
+        is_playable: false,
+        restrictions: {
+            reason: "Some reason",
         },
         name: "Track 1",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
+        popularity: 75,
+        preview_url: "https://example.com/track1/preview",
+        track_number: 1,
+        type: "track",
+        uri: "spotify:track:track1",
+        is_local: false,
+        audio_features: {
+            acousticness: "0.5",
+            analysis_url: "https://example.com/analysis1",
+            danceability: "0.8",
+            duration_ms: "240000",
+            energy: "0.7",
+            id: "audio1",
+            instrumentalness: "0.2",
+            liveness: "0.3",
+            loudness: "-6.5",
+            mode: "1",
+            speechiness: "0.05",
+            tempo: "120",
+            time_signature: "4/4",
+            track_href: "https://example.com/track1/audio",
+            type: "audio_features",
+            uri: "spotify:audio_features:audio1",
+            valence: "0.9",
+        },
     },
     {
-        id: "2",
         album: {
+            album_type: "album",
+            total_tracks: 15,
+            available_markets: ["US", "CA", "UK"],
+            external_urls: {
+                spotify: "https://example.com/album2",
+            },
+            href: "https://api.spotify.com/v1/albums/album2",
+            id: "album2",
             images: [],
             name: "Album 2",
+            release_date: "2023-02-01",
+            release_date_precision: "day",
+            restrictions: {
+                reason: "Some reason",
+            },
+            type: "album",
+            uri: "spotify:album:album2",
+            artists: [
+                {
+                    external_urls: {
+                        spotify: "https://example.com/artist2",
+                    },
+                    href: "https://api.spotify.com/v1/artists/artist2",
+                    id: "artist2",
+                    name: "Artist 2",
+                    type: "artist",
+                    uri: "spotify:artist:artist2",
+                },
+            ],
+        },
+        artists: [],
+        disc_number: 1,
+        duration_ms: 200000,
+        explicit: false,
+        external_urls: {
+            spotify: "https://example.com/track2",
+        },
+        href: "https://api.spotify.com/v1/tracks/track2",
+        id: "track2",
+        is_playable: false,
+        restrictions: {
+            reason: "Some reason",
         },
         name: "Track 2",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    {
-        id: "3",
-        album: {
-            images: [],
-            name: "Album 3",
+        popularity: 80,
+        preview_url: "https://example.com/track2/preview",
+        track_number: 1,
+        type: "track",
+        uri: "spotify:track:track2",
+        is_local: false,
+        audio_features: {
+            acousticness: "0.3",
+            analysis_url: "https://example.com/analysis2",
+            danceability: "0.7",
+            duration_ms: "200000",
+            energy: "0.8",
+            id: "audio2",
+            instrumentalness: "0.1",
+            liveness: "0.4",
+            loudness: "-5.5",
+            mode: "0",
+            speechiness: "0.08",
+            tempo: "130",
+            time_signature: "4/4",
+            track_href: "https://example.com/track2/audio",
+            type: "audio_features",
+            uri: "spotify:audio_features:audio2",
+            valence: "0.85",
         },
-        name: "Track 3",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
     },
-    {
-        id: "4",
-        album: {
-            images: [],
-            name: "Album 4",
-        },
-        name: "Track 4",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    {
-        id: "5",
-        album: {
-            images: [],
-            name: "Album 5",
-        },
-        name: "Track 5",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    {
-        id: "6",
-        album: {
-            images: [],
-            name: "Album 6",
-        },
-        name: "Track 6",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    {
-        id: "7",
-        album: {
-            images: [],
-            name: "Album 7",
-        },
-        name: "Track 7",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    {
-        id: "8",
-        album: {
-            images: [],
-            name: "Album 8",
-        },
-        name: "Track 8",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    {
-        id: "9",
-        album: {
-            images: [],
-            name: "Album 9",
-        },
-        name: "Track 9",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    {
-        id: "10",
-        album: {
-            images: [],
-            name: "Album 10",
-        },
-        name: "Track 10",
-        artists: [],
-        duration_ms: 0,
-        audio_features: undefined,
-    },
-    // Add 10 more entries as needed
+    // Repeat the structure for 8 more entries
 ];
 
 const responsesMock = {
--- frontend/src/commons/spotify/responsesTypes.ts
+++ frontend/src/commons/spotify/responsesTypes.ts
@@ -1,80 +1,80 @@
 interface image {
     url: string;
-    height: number,
-    width: number
+    height: number;
+    width: number;
 }
 
 export interface artist {
-    name: string,
-    images: image[],
+    name: string;
+    images: image[];
 
     external_urls: {
-        spotify: string
-    },
+        spotify: string;
+    };
     followers: {
-        href: string,
-        total: number
-    },
-    genres: string[],
-    href: string,
-    id: string,
-    popularity: number,
-    type: string,
-    uri: string
+        href: string;
+        total: number;
+    };
+    genres: string[];
+    href: string;
+    id: string;
+    popularity: number;
+    type: string;
+    uri: string;
 }
 
 export interface track {
     album: {
-        album_type: string,
-        total_tracks: number,
-        available_markets: string[],
+        album_type: string;
+        total_tracks: number;
+        available_markets: string[];
         external_urls: {
             spotify: string
-        },
-        href: string,
-        id: string,
-        images: image[],
-        name: string,
-        release_date: string,
-        release_date_precision: string,
+        };
+        href: string;
+        id: string;
+        images: image[];
+        name: string;
+        release_date: string;
+        release_date_precision: string;
         restrictions: {
-            reason: string
-        },
-        type: string,
-        uri: string,
+            reason: string;
+        };
+        type: string;
+        uri: string;
         artists: [
             {
                 external_urls: {
-                    spotify: string
-                },
-                href: string,
-                id: string,
-                name: string,
-                type: string,
-                uri: string
+                    spotify: string;
+                };
+                href: string;
+                id: string;
+                name: string;
+                type: string;
+                uri: string;
             }
         ]
-    },
-    artists: artist[],
-    disc_number: number,
-    duration_ms: number,
-    explicit: false,
+    };
+    artists: artist[];
+    disc_number: number;
+    duration_ms: number;
+    explicit: false;
     external_urls: {
-        spotify: string
-    },
-    href: string,
-    id: string,
-    is_playable: false,
+        spotify: string;
+    };
+    href: string;
+    id: string;
+    is_playable: false;
     restrictions: {
-        reason: string
-    },
-    name: string,
-    popularity: number,
-    preview_url: string,
-    track_number: number,
-    type: string,
-    uri: string,
-    is_local: false
+        reason: string;
+    };
+    name: string;
+    popularity: number;
+    preview_url: string;
+    track_number: number;
+    type: string;
+    uri: string;
+    is_local: false;
     audio_features?: audioFeatures;
 }
 
@@ -170,16 +170,16 @@ export interface trackWithAudioFeatures extends track {
 }
 
 export interface tracksDataItem {
-    added_at: string,
-    track: track
+    added_at: string;
+    track: track;
 }
 
 export interface tracksData {
-    href: string,
-    limit: number,
-    next: string,
-    offset: number,
-    previous: string,
-    total: number,
-    items: tracksDataItem[]
+    href: string;
+    limit: number;
+    next: string;
+    offset: number;
+    previous: string;
+    total: number;
+    items: tracksDataItem[];
 }
