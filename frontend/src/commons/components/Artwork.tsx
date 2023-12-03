3406c9a833045b3376b2f6cada29e163c2ffbda7
unknown <vincent.bernet@efrei.net>
2023-12-03 11:51:47 +0100

    feat: better accessibility & semantics for Artwork

--- frontend/src/commons/components/Artwork.tsx
+++ frontend/src/commons/components/Artwork.tsx
@@ -3,19 +3,20 @@ import { image } from "../spotify/responsesTypes";
 
 type ArtworkProps = {
     images: image[];
+    alt: string;
     isRounded?: boolean;
     size?: string;
 };
 
-const Artwork = ({ images, isRounded = false, size = "300px" }: ArtworkProps) => {
+const Artwork = ({ images, alt, isRounded = false, size = "300px" }: ArtworkProps) => {
     if (images.length === 0 || !images[0].url) {
         return (
-            <StyledArtwork src={'/images/default_image.png'} $isRounded={isRounded} $size={size} />
+            <StyledArtwork src={'/images/default_image.png'} $isRounded={isRounded} $size={size} alt={alt} />
         );
     }
 
     return (
-        <StyledArtwork src={images[0].url} $isRounded={isRounded} $size={size} />
+        <StyledArtwork src={images[0].url} $isRounded={isRounded} $size={size} alt={alt} />
     );
 }
 
@@ -26,6 +27,7 @@ type StyledArtworkProps = {
 
 const StyledArtwork = styled.img<StyledArtworkProps>`
     width: ${(props) => props.$size};
+    height: ${(props) => props.$size};
     border-radius: ${(props) => props.$isRounded ? "8px" : "0px"};
 `;
 
--- frontend/src/features/pages/track/Track.tsx
+++ frontend/src/features/pages/track/Track.tsx
@@ -86,6 +86,7 @@ const Track = () => {
                                 <div>
                                     <Artwork
                                         images={track.album.images}
+                                        alt={track.name}
                                         size={"300px"}
                                         isRounded
                                     />
@@ -113,6 +114,7 @@ const Track = () => {
                                         <div>
                                             <Artwork
                                                 images={tracksReco[indexReco].album.images}
+                                                alt={tracksReco[indexReco].name}
                                                 size={"300px"}
                                                 isRounded
                                             />
