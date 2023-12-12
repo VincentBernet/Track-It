2dec74673e3897e5827009bc33b6b1ac3812807b
Vincent Bernet <vincent.bernet@efrei.net>
2023-12-12 19:17:44 +0100

    release: add sort and search for tracks
    * feat: transform ul to table
    
    * feat: transform track list as a table with futur sorting options
    
    * feat: transform ul to table
    
    * feat: reformate trackCard as element of a table
    
    * Feat : Sort filter tracks
    
    * feat: transform ul to table
    
    * feat: reformate trackCard as element of a table
    
    * feat: improve render performance with better composition
    
    * feat: add sorting on the track tables
    
    * feat: search & filter your tracks
    
    * feat: transform ul to table
    
    * feat: reformate trackCard as element of a table
    
    * feat: improve render performance with better composition
    
    * feat: add sorting on the track tables
    
    * feat: reverse index if sort by spotify order
    
    * feat: add search feature for track table

--- frontend/src/commons/components/index.ts
+++ frontend/src/commons/components/index.ts
@@ -29,3 +29,4 @@ export { default as TrackCardList } from './TrackCardList';
 export { default as TrackList } from './TrackList';
 export { default as TrackCardOld } from './TrackCardOld';
 
+export { default as SearchFilter } from './SearchFilter';
