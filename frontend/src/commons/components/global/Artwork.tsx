6be00ef00bbc1e4820cef72933f30922998adf6c
Vincent Bernet <vincent@Vincent-Bernets-MacBook-Pro-KLQXRWLV9V.local>
2024-06-02 00:01:36 +0200

    feat: update package and fix bug

--- frontend/src/commons/components/global/Artwork.tsx
+++ frontend/src/commons/components/global/Artwork.tsx
@@ -1,31 +1,50 @@
-import styled from "styled-components";
-import type { imageType } from "../../spotify/responsesTypes";
+import styled from 'styled-components'
+import type { imageType } from '../../spotify/responsesTypes'
+import { ErrorBoundary, ErrorBoundaryContext } from 'react-error-boundary'
 
 type ArtworkProps = {
-	images: imageType[];
-	alt: string;
-	isRounded?: boolean;
-	size?: string;
-};
+  images: imageType[]
+  alt: string
+  isRounded?: boolean
+  size?: string
+}
 
 // TODO : Add a url parameter to redirect to the spotify page (for artist, tracks and playlist) test
-const Artwork = ({ images, alt, isRounded = false, size = "300px" }: ArtworkProps) => {
-	if (images.length === 0 || !images[0].url) {
-		return <StyledArtwork src={"/images/default_image.png"} $isRounded={isRounded} $size={size} alt={alt} />;
-	}
-
-	return <StyledArtwork src={images[0].url} $isRounded={isRounded} $size={size} alt={alt} />;
-};
+const Artwork = ({
+  images,
+  alt,
+  isRounded = false,
+  size = '300px',
+}: ArtworkProps) => {
+  if (images === null) {
+    return (
+      <StyledArtwork
+        src={'/images/default_image.png'}
+        $isRounded={isRounded}
+        $size={size}
+        alt={alt}
+      />
+    )
+  }
+  return (
+    <StyledArtwork
+      src={images[0].url}
+      $isRounded={isRounded}
+      $size={size}
+      alt={alt}
+    />
+  )
+}
 
 type StyledArtworkProps = {
-	$isRounded: boolean;
-	$size: string;
-};
+  $isRounded: boolean
+  $size: string
+}
 
 const StyledArtwork = styled.img<StyledArtworkProps>`
-    width: ${(props) => props.$size};
-    height: ${(props) => props.$size};
-    border-radius: ${(props) => (props.$isRounded ? "8px" : "0px")};
-`;
+  width: ${(props) => props.$size};
+  height: ${(props) => props.$size};
+  border-radius: ${(props) => (props.$isRounded ? '8px' : '0px')};
+`
 
-export default Artwork;
+export default Artwork
